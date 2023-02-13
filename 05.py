bd0a307007eecd2908fa71215e6d3475abb40813
peter123321123321 <98923575+peter123321123321@users.noreply.github.com>
2023-02-14 11:58:46 +1300

    changes

--- 05.py
+++ 05.py
@@ -1,17 +1,35 @@
 total_marks = 0
 total_students = 0
 best_mark = 0
-average_mark = list("name", "mark", "grade")
+average_mark = list()
 best_student = ""
+students = list()
 while True:
     name = input("What is the students name?").lower()
     if name == "x":
         print(f"The average mark is {sum(average_mark)/len(average_mark)}, The best mark is {best_mark} "
               f"and the best student is {best_student}")
+        print(students)
         break
     else:
         mark = int(input("What is the students mark [1-100] "))
-        average_mark.append(mark)
-        if mark >= best_mark:
-            best_mark = mark
-            best_student = name
+        if mark == range(90, 100):
+            mark = "A+"
+        elif mark == range(85, 89):
+            mark = "A"
+        elif mark == range(80, 84):
+            mark = "A-"
+        elif mark == range(75, 79):
+            mark = "B+"
+        elif mark == range(70, 74):
+            mark = "B"
+        elif mark == range(65, 69):
+            mark = "B-"
+        elif mark == range(60, 64):
+            mark = "C+"
+
+    students.append(name, mark)
+    average_mark.append(mark)
+    if mark >= best_mark:
+        best_mark = mark
+        best_student = name
